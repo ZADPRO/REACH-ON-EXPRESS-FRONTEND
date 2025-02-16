bcfc71a1bfae02b7574ccdf7b98909b0a7540244
IONHAX04 <thirukumara04@gmail.com>
2025-02-16 10:43:22 +0530

    login route drafted

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import {
   Routes,
   Route,
   useLocation,
+  Navigate,
 } from "react-router-dom";
-
 import PropTypes from "prop-types";
 
 import Header from "./components/01-Header/Header";
@@ -17,27 +17,96 @@ import Profile from "./components/07-Profile/Profile";
 import Login from "./components/08-Login/Login";
 import Booking from "./components/09-Booking/Booking";
 import Report from "./components/10-Report/Report";
-
-import "./App.css";
 import TestingPDF from "./components/11-TestingPDF/TestingPDF";
 import ReportPDF from "./components/12-ReportPDF/ReportPDF";
+
+import "./App.css";
+
+function PrivateRoute({ children }) {
+  const userDetails = localStorage.getItem("userDetails");
+  return userDetails ? children : <Navigate to="/login" replace />;
+}
 
 function App() {
   return (
     <Router>
       <ConditionalHeader>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/booking" element={<Booking />} />
-          <Route path="/mapping" element={<Key />} />
-          <Route path="/employee" element={<Employees />} />
-          <Route path="/tracking" element={<Tracking />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/report" element={<Report />} />
           <Route path="/login" element={<Login />} />
           <Route path="/testingPDF" element={<TestingPDF />} />
-          <Route path="/reportPDF" element={<ReportPDF />} />
+
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/booking"
+            element={
+              <PrivateRoute>
+                <Booking />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/mapping"
+            element={
+              <PrivateRoute>
+                <Key />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/employee"
+            element={
+              <PrivateRoute>
+                <Employees />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/tracking"
+            element={
+              <PrivateRoute>
+                <Tracking />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/settings"
+            element={
+              <PrivateRoute>
+                <Settings />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute>
+                <Profile />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/report"
+            element={
+              <PrivateRoute>
+                <Report />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/reportPDF"
+            element={
+              <PrivateRoute>
+                <ReportPDF />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </ConditionalHeader>
     </Router>
@@ -46,7 +115,6 @@ function App() {
 
 function ConditionalHeader({ children }) {
   const location = useLocation();
-
   const excludedRoutes = ["/login", "/testingPDF"];
   const isExcluded = excludedRoutes.includes(location.pathname);
 
